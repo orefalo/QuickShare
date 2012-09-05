7212bdbb9b23b8c28cff6c30a64bb8f7a4ec86f5
Olivier Refalo <orefalo@yahoo.com>
2012-09-04 23:54:36 -0400

    migration to binaryjs:part1

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
 module.exports = function () {
 
 	var path = require('path');
-	var express = require('express')
-		, config = require(path.join(__dirname, 'conf', 'general.js'));
+	var http = require('http');
+	var express = require('express');
+	var config = require(path.join(__dirname, 'conf', 'general.js'));
 
 	var app = express();
 
 	require(path.join(__dirname, 'environments.js'))(express, app, config);
 	require(path.join(__dirname, 'routes.js'))(app, config);
 
-	var server = app.listen(app.get('port'), function () {
-		console.log("Express " + app.get('env') + " server listening on port " + app.get('port'));
-	});
+	var httpserver = http.createServer(app);
 
-	require(path.join(__dirname, 'server-iosockets.js'))(server);
+	var BinaryServer = require('binaryjs').BinaryServer;
+	var bs = BinaryServer({server: httpserver});
+
+
+	httpserver.listen(app.get('port'));
+	console.log("Express " + app.get('env') + " server listening on port " + app.get('port'));
+
+
+
+
+
+//	require(path.join(__dirname, 'server-iosockets.js'))(server);
 
 
 };
