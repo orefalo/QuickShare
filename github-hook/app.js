53fd7be272c4b5d7836b1e4341ba3949ddc2992f
Olivier Refalo <orefalo@yahoo.com>
2012-09-21 01:30:45 -0400

    the hook

--- github-hook/app.js
+++ github-hook/app.js
@@ -1,8 +1,21 @@
-var gith = require('gith').create(9999);
+var githubhook = require('githubhook'),
+	servers = {
+		// that's the url webhook to define in github
+		// http://www.ugot.com:9999/472398FE-AB54-413A-93F0-3E1918F966F1
+		'472398FE-AB54-413A-93F0-3E1918F966F1':'https://github.com/orefalo/QuickShare'
+	};
 
-gith({
-	repo:'git@github.com:orefalo/QuickShare.git',
-	branch:/PROD/
-}).on('file:all', function (payload) {
-	console.log('Post-receive happened!');
-}); 
+var thishook = githubhook(9999, servers, function (err, payload) {
+	if (!err) {
+
+		// console.log(payload);
+
+		if (payload.ref === "refs/heads/PROD") {
+			console.log("GOOD");
+
+			//TODO: trigger a shell script
+		}
+	} else {
+		console.log(err);
+	}
+});
