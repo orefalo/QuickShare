13616eb96c832d2b559aa75d1c79187e0318d544
Olivier Refalo <orefalo@yahoo.com>
2012-09-23 22:55:17 -0400

    fix path issue

--- githubhook/app.js
+++ githubhook/app.js
@@ -1,9 +1,12 @@
-var githubhook = require('./githubhook.js'),
-	servers = {
-		// that's the url webhook to define in github, generate id with uuidgen
-		// http://www.ugot.com:9999/472398FE-AB54-413A-93F0-3E1918F966F1
-		'79a96853-5920-4654-935b-b4e778394f52':'https://github.com/orefalo/QuickShare'
-	};
+var githubhook = require('./githubhook.js');
+var path = require('path');
+var exec = require('child_process').exec;
+
+var servers = {
+	// that's the url webhook to define in github, generate id with uuidgen
+	// http://www.ugot.com:9999/472398FE-AB54-413A-93F0-3E1918F966F1
+	'79a96853-5920-4654-935b-b4e778394f52':'https://github.com/orefalo/QuickShare'
+};
 
 var thishook = githubhook(9999, servers, function (err, payload) {
 	if (!err) {
@@ -14,12 +17,12 @@ var thishook = githubhook(9999, servers, function (err, payload) {
 		if (payload.ref === "refs/heads/PROD") {
 			console.log("Processing...");
 
-			var exec = require('child_process').exec;
-			var child = exec("./hook.sh", function (error, stdout, stderr) {
+
+			var child = exec(path.join(__dirname, "./hook.sh"), function (error, stdout, stderr) {
 				var result = "stdout:" + stdout;
-				if(stderr) {
-					result+= "\nstderr:";
-					result+=stderr;
+				if (stderr) {
+					result += "\nstderr:";
+					result += stderr;
 				}
 				console.log(result);
 			});
