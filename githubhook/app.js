d40d4b635a5cc5fa578810f8567d4ebe4fa913a3
Olivier Refalo <orefalo@yahoo.com>
2012-09-23 22:41:20 -0400

    launch using exec

--- githubhook/app.js
+++ githubhook/app.js
@@ -9,15 +9,23 @@ var thishook = githubhook(9999, servers, function (err, payload) {
 	if (!err) {
 
 		// console.log(payload);
-                console.log("Branch "+payload.ref);
+		console.log("Branch " + payload.ref);
 
 		if (payload.ref === "refs/heads/PROD") {
 			console.log("GOOD");
 
-			var spawn = require('child_process').spawn
-			spawn('./hook.sh');
+//			var spawn = require('child_process').spawn
+//			spawn('./hook.sh');
+
+			var exec = require('child_process').exec;
+			var child = exec("./hook.sh", function (error, stdout, stderr) {
+				var result = '{"stdout":' + stdout + ',"stderr":"' + stderr + '","cmd":"' + cmd + '"}';
+				console.log(result);
+			});
+
+
+		} else {
+			console.log(err);
 		}
-	} else {
-		console.log(err);
 	}
 });
