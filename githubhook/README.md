fce9bc146fc63fe6f79cc7df05f0db816a3883c9
Olivier Refalo <orefalo@yahoo.com>
2012-09-23 22:00:25 -0400

    https hook with express 3

---
+++ githubhook/README.md
@@ -0,0 +1,9 @@
+
+1. gen a uuid with bash `uuidgen` command
+2. define the uuid in app.js
+3. defien port in app.js
+4. setup github hooks as URL https://myserver.domain:port/uuid
+5. generate ssl certificate by calling `gen_ssl_cert`
+6. `npm install`
+7. `node app.js`
+
--- githubhook/app.js
+++ githubhook/app.js
@@ -1,15 +1,15 @@
-var githubhook = require('githubhook'),
+var githubhook = require('./githubhook.js'),
 	servers = {
-		// that's the url webhook to define in github
+		// that's the url webhook to define in github, generate id with uuidgen
 		// http://www.ugot.com:9999/472398FE-AB54-413A-93F0-3E1918F966F1
-		'472398FE-AB54-413A-93F0-3E1918F966F1':'https://github.com/orefalo/QuickShare'
+		'79a96853-5920-4654-935b-b4e778394f52':'https://github.com/orefalo/QuickShare'
 	};
 
 var thishook = githubhook(9999, servers, function (err, payload) {
 	if (!err) {
 
 		// console.log(payload);
-		console.log("Branch "+payload.ref);
+                console.log("Branch "+payload.ref);
 
 		if (payload.ref === "refs/heads/PROD") {
 			console.log("GOOD");
